30b2b7105623e017e6521910059219633ae55665
jobootwori <30703963+jobootwori@users.noreply.github.com>
2025-02-07 13:48:27 +0300

    Update DJANGO_SETTINGS_MODULE path in manage.py for correct module reference

--- digital_stamping_backend/manage.py
+++ digital_stamping_backend/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digital_stamping_backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digital_stamping_backend.digital_stamping_backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
